f5d9dc9d454cbccb4a17a25be64bfbd5d0901c4d
manlok666 <17wlqiu@gmail.com>
2020-09-02 18:19:23 +0800

    fixed

--- back_end/controllers/UserController.php
+++ back_end/controllers/UserController.php
@@ -1,12 +1,22 @@
 <?php
 namespace app\controllers;
 
-
 use app\services\UserService;
 use yii\web\Response;
-
+/**
+ * TestController 用户控制器
+ *
+ * @author manlok <17wlqiu@gmail.com>
+ * @since 0.1
+ */
 class UserController extends BaseAPIController
 {
+    /**
+     * 用户登录
+     * @param string name 用户名
+     * @param string password 密码
+     * @return string 登录结果
+     */
     public function actionLogin()
     {
         \Yii::$app->response->format = Response::FORMAT_JSON;
